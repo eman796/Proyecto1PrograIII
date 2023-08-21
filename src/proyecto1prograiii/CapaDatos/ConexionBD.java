c49b3530625819ecdf8161c29c1cc5ff896a7254
Main <emanuelcalvopizarro0123@gmail.com>
2023-08-21 12:25:40 -0600

    Emanu

--- src/proyecto1prograiii/CapaDatos/ConexionBD.java
+++ src/proyecto1prograiii/CapaDatos/ConexionBD.java
@@ -3,15 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto1prograiii.CapaDatos;
-import com.mysql.cj.jdbc.Driver;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author emanu
+ * @author Franco
  */
 public class ConexionBD {
-    n
+    Connection con = null;
+    
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String usuario = "root";
+    private static String password = "UZgXbVtuZHm4gdf6EO0g";
+    private static String url = "jdbc:mysql://containers-us-west-148.railway.app:6678/_PERSONAS?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    
+    static
+    {
+        try {
+            Class.forName(driver);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.toString(), "Project", 0);
+        }
+    
+    }
+    
+    public Connection getConn()
+    {
+        try {
+            con =  DriverManager.getConnection(url,usuario,password);
+        } catch (Exception e) {
+            System.out.println("Error al conectarnos a la base de datos");
+            con = null;
+        }
+    return con;
+    }
+    
 }
